366b2ad3c34a98bec64c0bae949f695e2fc5f74e
CamChavez <124646465+CamChavez@users.noreply.github.com>
2023-02-23 16:31:14 -0600

    Add files via upload

---
+++ TAREAGATOCADENA.java
@@ -0,0 +1,26 @@
+public class cadenagato  {
+
+   public static void main(String[] args){
+
+        String cadena = "OmXmXmXmOm_m_m_mO";
+        String[] mt = cadena.split("m");
+                String morat0 = mt[0];
+                String morat1 = mt[1];
+                String morat2 = mt[2];
+                String morat3 = mt[3];
+                String morat4 = mt[4];
+                String morat5 = mt[5];
+                String morat6 = mt[6];
+                String morat7 = mt[7];
+                String morat8 = mt[8];
+
+        System.out.println("-------------");
+        System.out.println("| " + mt[0] + " | " + mt[1] + " | " + mt[2] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + mt[3] + " | " + mt[4] + " | " + mt[5] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + mt[6] + " | " + mt[7] + " | " + mt[8] + " |");
+        System.out.println("-------------");
+
+        }
+}
