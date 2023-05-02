9069db824afb2ebb58af773d8cd6e23ea09bce7a
CamChavez <124646465+CamChavez@users.noreply.github.com>
2023-05-01 21:08:44 -0600

    Add files via upload

---
+++ TAREA12/Main.java
@@ -0,0 +1,10 @@
+import Iu.Principal;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("HOLA MUNDO AWT");
+
+    }
+}
